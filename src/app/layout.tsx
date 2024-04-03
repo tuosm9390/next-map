e3b29cc985e03efea136baf4272976c6762308be
tuosm9390 <tuosm123@naver.com>
2024-04-03 18:52:57 +0900

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import GoogleAnalytics from "./googleAnalytics";
 import { NextLayout, NextProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Fastcampus NextMap",
   description: "Next.js 13을 이용한 맛집 앱",
+  openGraph: {
+    title: "Fastcampus NextMap",
+    description: "Next.js 13을 이용한 맛집 앱",
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_ID} />
         <NextProvider>
           <NextLayout>{children}</NextLayout>
         </NextProvider>
